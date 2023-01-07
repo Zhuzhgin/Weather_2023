25972cf455262d5b53d24b3cb962606b349f9e5e
Zhuzhgin <artemfox@list.ru>
2023-01-07 18:26:58 +0500

    Initial Commit

---
+++ Weather_2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather_2023
+//
+//  Created by Artem Zhuzhgin on 07.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
